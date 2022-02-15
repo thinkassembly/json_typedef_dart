317f7520f652cdcbb6f04a574d534ee6e258cc20
nullrocket <douglas.b.hill@gmail.com>
2022-02-14 20:44:15 -0500

    Initial Commit

--- lib/src/validate.dart
+++ lib/src/validate.dart
@@ -2,8 +2,30 @@
 import 'package:json_typedef_dart/src/schema.dart';
 import 'package:json_typedef_dart/src/types.dart';
 
-ValidationErrors validate({required Schema schema, required Json data}) {
+class ValidationError {
+  List<String> instancePath;
+  List<String> schemaPath;
+  ValidationError({required this.instancePath,required this.schemaPath});
+}
+
+class ValidationState {
+List<ValidationError> errors = [];
+List<String> instanceTokens =[];
+List<List<String>> schemaTokens = [];
+Json root ;
+int maxDepth;
+int maxErrors;
+ValidationState({required this.root,this.maxDepth = 0,this.maxErrors = 0});
+}
+ValidationErrors validate({required Json schema, required dynamic data,int maxDepth=0, int maxErrors=0}) {
+
+
 
 
   return [];
+}
+
+
+void validateWithState() {
+
 }
--- test/json_typedef_dart_test.dart
+++ test/json_typedef_dart_test.dart
@@ -4,13 +4,29 @@ import 'package:json_typedef_dart/json_typedef_dart.dart';
 import 'package:json_typedef_dart/src/types.dart';
 
 import 'invalid_schemas.dart';
+import 'schema_validation.dart';
 
 void main() {
-  group("Schema Validation",(){
-   for(var testCase in InvalidSchemas.entries) {
-    test(testCase.key,(){
-      expect(isValidSchema(testCase.value as Json ),false);
-    });
-   }
+  group("Schema Validation", () {
+    for (var testCase in InvalidSchemas.entries) {
+      test(testCase.key, () {
+        expect(isValidSchema(testCase.value as Json), false);
+      });
+    }
+  });
+
+  group("JSON Validation", () {
+    for (var testCase in TestSchemas.entries) {
+      test(testCase.key, () {
+        print(testCase.value["schema"]);
+        print(testCase.value["instance"]);
+        print(testCase.value["errors"]);
+        expect(
+            validate(
+                schema: testCase.value["schema"] as Json,
+                data: testCase.value["instance"]),
+            testCase.value["errors"]);
+      });
+    }
   });
 }
