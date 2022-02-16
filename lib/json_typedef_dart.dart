c4929014a7ef5271b4b876c8cc14a589a4d8650c
nullrocket <douglas.b.hill@gmail.com>
2022-02-16 15:10:27 -0500

    Initial Commit

--- lib/json_typedef_dart.dart
+++ lib/json_typedef_dart.dart
@@ -1,5 +1,5 @@
 library json_typedef_dart;
 
-export './src/validate.dart';
+export './src/validate.dart' show validate;
 export './src/schema.dart' show isValidSchema ;
 export './src/types.dart' show Json;
